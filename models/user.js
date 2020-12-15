ed1deab5fec9ce3c819bda6246523647ed501749
Lawson Kelly <wlawsonkelly@gmail.com>
2020-12-15 13:01:41 -0500

    ready for day 2

--- models/user.js
+++ models/user.js
@@ -27,6 +27,10 @@ module.exports = function(sequelize, DataTypes) {
       validate: {
         len: [8,28]
      }
+    },
+    emailable: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
     }
   });
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
--- public/assets/js/signup.js
+++ public/assets/js/signup.js
@@ -8,17 +8,19 @@ $(document).ready(function() {
     // When the signup button is clicked, we validate the email and password are not blank
     signUpForm.on("submit", function(event) {
       event.preventDefault();
+      let emailAble = confirm("Do you want to get emails about new lessons from us?");
       var userData = {
         email: emailInput.val().trim(),
         username: usernameInput.val().trim(),
-        password: passwordInput.val().trim()
+        password: passwordInput.val().trim(),
+        emailable: emailAble
       };
   
       if (!userData.email || !userData.username || !userData.password) {
         return;
       }
       // If we have an email and password, run the signUpUser function
-      signUpUser(userData.email, userData.username, userData.password);
+      signUpUser(userData.email, userData.username, userData.password, userData.emailable);
       emailInput.val("");
       usernameInput.val("");
       passwordInput.val("");
@@ -26,11 +28,12 @@ $(document).ready(function() {
   
     // Does a post to the signup route. If successful, we are redirected to the members page
     // Otherwise we log any errors
-    function signUpUser(email, username, password) {
+    function signUpUser(email, username, password, emailable) {
       $.post("/api/signup", {
         email: email,
         username: username,
-        password: password
+        password: password,
+        emailable: emailable
       })
         .then(function(data) {
           window.location.replace("/home");
